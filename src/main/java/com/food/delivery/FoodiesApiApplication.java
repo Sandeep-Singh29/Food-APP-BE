4f86903f3cb456f546d75d2f04cf8c28ca34cfe5
Sandeep <singh29july95@gmail.com>
2025-03-24 13:49:58 +0530

    first commit BE Code

--- src/main/java/com/food/delivery/FoodiesApiApplication.java
+++ src/main/java/com/food/delivery/FoodiesApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodiesApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodiesApiApplication.class, args);
+		SpringApplication. run(FoodiesApiApplication.class, args);
 	}
 
 }
--- src/main/java/com/food/delivery/controller/FoodController.java
+++ src/main/java/com/food/delivery/controller/FoodController.java
@@ -24,6 +24,7 @@ import java.util.List;
 @RequestMapping("/api/foods")
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class FoodController {
 
     private final FoodService foodService;
